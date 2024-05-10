42a3ddba49d5e9fa4b9bf8a73e29cc6fb9cdcff6
Pnaga90 <167768235+Pnaga90@users.noreply.github.com>
2024-05-10 13:55:35 +0530

    Add files via upload

---
+++ myHonda.java
@@ -0,0 +1,12 @@
+package general;
+
+public class myHonda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		honda mine = new honda();
+		mine.run();
+		
+	}
+
+}
