a656d1329d568f84c26d52fc4b59cf354ee15894
Pnaga90 <167768235+Pnaga90@users.noreply.github.com>
2024-05-10 13:40:44 +0530

    Add files via upload

---
+++ firstClass.java
@@ -0,0 +1,13 @@
+package TestCases;
+
+public class firstClass {
+	public void a() {
+		System.out.println("i am in first class method a");
+	}
+	
+public void b() {
+	
+System.out.println("I am in first class method b");
+}
+	
+}
