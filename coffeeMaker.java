13f860016da4ac32b9eca10bee75e4d44c5284a5
Pnaga90 <167768235+Pnaga90@users.noreply.github.com>
2024-05-10 13:50:11 +0530

    Add files via upload

---
+++ coffeeMaker.java
@@ -0,0 +1,20 @@
+package general;
+
+public class coffeeMaker implements plugPoint {
+    int capacity;
+	@Override
+	public void dry() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void brew() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Brewing coffee");
+	}
+	
+	
+
+}
